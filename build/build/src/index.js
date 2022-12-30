b2e088e01244f76d4bffeb37f7f11f8bbf7bd1d6
ayaahmed281 <AyaAhmed2017@outlook.com>
2022-12-30 20:02:54 +0200

    test

---
+++ build/build/src/index.js
@@ -0,0 +1,47 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const body_parser_1 = __importDefault(require("body-parser"));
+const express_1 = __importDefault(require("express"));
+const order_1 = __importDefault(require("./handelers/order"));
+const product_1 = __importDefault(require("./handelers/product"));
+const user_1 = __importDefault(require("./handelers/user"));
+const app = (0, express_1.default)();
+app.listen("54242", () => {
+    console.log(`Server is starting at prot:${54242}`);
+});
+app.use(body_parser_1.default.json());
+app.get("/", function (req, res) {
+    // BookStore.index();
+    res.send("hello world");
+});
+app.use("/api", product_1.default);
+app.use("/api", user_1.default);
+app.use("/api", order_1.default);
+// app.get("/books", function (req, res) {
+//   try {
+//     res.send("get all books")
+//   } catch (err) {
+//     res.status(400)
+//     res.json(err)
+//   }
+// })
+// app.get("/books/:id", function (req, res) {
+//   try {
+//     res.send("get book by id")
+//   } catch (err) {
+//     res.status(400)
+//     res.json(err)
+//   }
+// })
+// app.delete("/books", function (req, res) {
+//   try {
+//     res.send("delete all books")
+//   } catch (err) {
+//     res.status(400)
+//     res.json(err)
+//   }
+// })
+exports.default = app;
---
+++ build/src/handelers/product.js
@@ -0,0 +1,94 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const product_1 = require("../models/product");
+const helpers_1 = require("../helpers");
+const router = express_1.default.Router();
+const store = new product_1.Products();
+router.get("/products", (_req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const products = yield store.index();
+        res.json(products);
+    }
+    catch (err) {
+        res.json(err);
+    }
+}));
+router.get("/products/:id", (_req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const Product = yield store.show(parseInt(_req.params.id, 1));
+        res.json(Product);
+    }
+    catch (err) {
+        res.json(err);
+    }
+}));
+router.post("/products", helpers_1.authToken, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a;
+    try {
+        const Product = {
+            price: req.body.price,
+            pName: req.body.name,
+            category: (_a = req.body) === null || _a === void 0 ? void 0 : _a.category
+        };
+        yield store.create(Product).then((_res) => {
+            res.json({
+                data: Product,
+                statusCode: 200,
+                message: "Done."
+            });
+        });
+    }
+    catch (err) {
+        res.json({
+            statusCode: 400,
+            message: "an error occurred.",
+            data: err
+        });
+    }
+}));
+router.put("/products/:id", helpers_1.authToken, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const Product = {
+            category: req.body.category,
+            price: req.body.price,
+            pName: req.body.name
+        };
+        yield store.update(Product, parseInt(req.params.id, 1)).then(() => {
+            res.json({
+                statusCode: 200,
+                message: "Done.",
+                data: Product
+            });
+        });
+    }
+    catch (err) {
+        res.json(err);
+    }
+}));
+router.delete("/products/:pid", helpers_1.authToken, (_req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        yield store.delete(parseInt(_req.params.pid, 1)).then(() => {
+            res.json({
+                message: "Done.",
+                statusCode: 200
+            });
+        });
+    }
+    catch (err) {
+        res.json(err);
+    }
+}));
+exports.default = router;
